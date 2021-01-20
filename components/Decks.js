354d1ec26237deee0c0a5baa4e2e4fea80fdd1aa
Farhan Ul Haq <farhan@openvpn.net>
2021-01-20 10:59:20 -0800

    feat: implement UI for displaying Decks

--- components/Decks.js
+++ components/Decks.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react'
-import { View, Text } from "react-native"
+import { View, Text } from 'react-native'
+import { fetchCalendarResults } from '../utils/api'
+import { connect } from 'react-redux'
+import { receiveEntries, addEntry } from '../actions'
+import { ListItem } from 'react-native-elements'
 
 class Decks extends Component {
+  componentDidMount () {
+    const { dispatch } = this.props
+    fetchCalendarResults()
+      .then((entries) => dispatch(receiveEntries(entries)))
+  }
+
   render() {
+    const { entries } = this.props
     return (
       <View>
-        <Text>Decks</Text>
+        {Object.keys(entries).map((deck, i) => (
+          <ListItem key={i} bottomDivider>
+            <ListItem.Content>
+              <ListItem.Title>{entries[deck].title}</ListItem.Title>
+              <ListItem.Subtitle>{entries[deck].questions.length} Cards</ListItem.Subtitle>
+            </ListItem.Content>
+            <ListItem.Chevron />
+          </ListItem>
+          ))}
       </View>
     )
   }
 }
 
-export default Decks
+function mapStateToProps(entries) {
+  return {
+    entries
+  }
+}
+export default connect(mapStateToProps)(Decks)
