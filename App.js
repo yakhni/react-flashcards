cdbdb51a22cc0c5d7e9471d0d281a00fcf4ccc7d
Farhan Ul Haq <farhan@openvpn.net>
2021-01-20 10:54:07 -0800

    feat: add redux, react-redux, actions, reducers, middleware.

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import * as React from "react"
 import { View } from "react-native"
 import { NavigationContainer } from '@react-navigation/native';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import RootStack from './components/Stack'
 import StatusBar from './components/StatusBar'
+import Tabs from './components/Tabs'
+import reducers from './reducers'
+import middleware from './middleware'
+
+const store = createStore(reducers, middleware)
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar />
-      <View style={{flex: 1}}>
-        <RootStack />
-      </View>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <StatusBar />
+        <View style={{flex: 1}}>
+          <RootStack />
+        </View>
+      </NavigationContainer>
+    </Provider>
   )
 }
---
+++ actions/index.js
@@ -0,0 +1,15 @@
+export const ADD_ENTRY = 'ADD_ENTRY'
+export const RECEIVE_ENTRIES = 'RECEIVE_ENTRIES'
+
+export function receiveEntries (entries) {
+  return {
+    type: RECEIVE_ENTRIES,
+    entries
+  }
+}
+export function addEntry (entry) {
+  return {
+    type: ADD_ENTRY,
+    entry
+  }
+}
