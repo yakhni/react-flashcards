9925fbc86e6d0313e3474f353561ab655a2d230e
Farhan Ul Haq <farhan@openvpn.net>
2021-01-21 11:21:43 -0800

    feat: add Component for creating new card

---
+++ components/NewCard.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import { Text } from 'react-native-elements'
+import Input from './Input'
+import Button from './Button'
+import { connect } from 'react-redux'
+import { addCard } from '../actions'
+import styles from '../utils/styles'
+
+class NewCard extends Component {
+  state = {
+    question: '',
+    answer: ''
+  }
+
+  handleOnPress = () => {
+    const { add, goBack } = this.props
+    add(this.state)
+    goBack()
+  }
+
+  render() {
+    return (
+      <View style={styles.mainView}>
+        <Text h4>Some text goes here</Text>
+
+        <Input placeholder='Question goes here'
+               onChangeText={(question) => this.setState({question})}
+
+               value={this.state.question}/>
+        <Input placeholder='Answer goes here'
+               onChangeText={(answer) => this.setState({answer})}
+
+               value={this.state.answer}/>
+      <Button title='Add' onPress={this.handleOnPress}/>
+      </View>
+    )
+  }
+}
+
+function mapStateToProps(state, { route }) {
+  const { entryId } = route.params;
+  return {
+    entryId
+  }
+}
+
+function mapDispatchToProps(dispatch, {route, navigation}) {
+  const { entryId } = route.params;
+    return {
+      add: (entry) => dispatch(addCard(entryId, entry)),
+      goBack: () => navigation.goBack()
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewCard)
