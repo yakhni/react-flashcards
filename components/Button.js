ec34b532ca28dc21fef319e70e099b9efe596f6b
Farhan Ul Haq <farhan@openvpn.net>
2021-01-21 11:22:32 -0800

    feat: create Button Component that wraps around Button from react-native-elements

---
+++ components/Button.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { Button as NativeButton } from 'react-native-elements';
+
+function Button ({ title, style = {}, ...rest }) {
+  return (
+    <NativeButton type='solid' title={title} buttonStyle={[styles.button, style]} {...rest} />
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: 200,
+  }
+})
+
+export default Button
