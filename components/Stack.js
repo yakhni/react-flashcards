0b49a1a5e45ee69eba3c0616481e42a484137ec7
Farhan Ul Haq <farhan@openvpn.net>
2021-01-21 11:19:28 -0800

    feat: add back navigation to Deck, NewCard

--- components/Stack.js
+++ components/Stack.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Tabs from './Tabs'
+import NewCard from './NewCard'
+import Deck from './Deck'
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator()
@@ -7,6 +9,8 @@ export default function RootStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name='Decks' component={Tabs} />
+      <Stack.Screen name='Deck' component={Deck} />
+      <Stack.Screen name='NewCard' component={NewCard} />
     </Stack.Navigator>
   )
 }
